7a2691b34cd40a60592547f147d5ad7cc63690bd
Kevin Scott <kevinjscott@Kevins-MacBook-Pro.local>
2016-05-19 11:24:34 -0400

    tech tip detail view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('cortex.controllers', [])
 
-.controller('DemoCtrl', function($scope, $http, $ionicSideMenuDelegate, $ionicModal, Users, $ionicLoading, $state, $timeout) {
+.controller('DemoCtrl', function($scope, $http, $ionicSideMenuDelegate, $ionicModal, $ionicHistory, Users, $ionicLoading, $state, $timeout) {
 
   $scope.users = Users.all();
 
@@ -25,39 +25,6 @@ angular.module('cortex.controllers', [])
       $scope.closeRegister();
     }, 1000);
   }
-
-
-
-
-
-
-//   $scope.groups = [];
-//   for (var i=0; i<10; i++) {
-//     $scope.groups[i] = {
-//       name: i + 1,
-//       items: []
-//     };
-//     for (var j=0; j<3; j++) {
-//       $scope.groups[i].items.push(" Course " + (i+1) + ', Module ' + (j+1));
-//     }
-//   }
-  
-//   $scope.toggleGroup = function(group) {
-//     if ($scope.isGroupShown(group)) {
-//       $scope.shownGroup = null;
-//     } else {
-//       $scope.shownGroup = group;
-//     }
-//   };
-//   $scope.isGroupShown = function(group) {
-//     return $scope.shownGroup === group;
-//   };
-
-
-
-
-
-
 
 
   $scope.actionSheet = function() {
