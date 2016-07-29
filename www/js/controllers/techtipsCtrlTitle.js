ad12ae9cc2012832582f2bb51db980263ca29c3a
surojit-pakira <surojit_pakira@infosys.com>
2016-07-29 09:45:29 -0400

    Enhanced Search API and Cable lab API

--- www/js/controllers/techtipsCtrlTitle.js
+++
@@ -1,8 +0,0 @@
-var TechtipsCtrlTitle = ['$scope', '$rootScope', function($scope, $rootScope) {
-
-    $scope.fetchData = function() {
-	    $scope.title= $rootScope.globalTitle;
-	    $scope.content=$rootScope.globalContent;
-    };
-    
-}];
