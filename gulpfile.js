63bc384bce04178b1ea66a17f566574b8b29d2a1
Kevin J. Scott <kevinjscott@gmail.com>
2016-10-05 09:00:55 -0400

    clean client libs to make deploy size small

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var del = require('del');
 
 var paths = {
   sass: ['./scss/**/*.scss', './scss/**/*.sass']
@@ -48,4 +49,109 @@ gulp.task('git-check', function(done) {
     process.exit(1);
   }
   done();
+});
+
+gulp.task('clean-client-libs', function () {
+  return del([
+    // specific directories
+    'www/lib/angular',
+    'www/lib/angular-animate',
+    'www/lib/angular-sanitize',
+    'www/lib/angular-ui-router',
+    'www/lib/ionic-ion-tinder-cards',
+    'www/lib/platform.js',
+    'www/lib/require.js',
+    'www/lib/requirejs',
+    'www/lib/requirejs-text',
+
+    // multiple directories
+    'www/lib/**/src/*',
+    '!www/lib/tabletop/src/tabletop.js',
+
+    'www/lib/**/test',
+    'www/lib/**/tests',
+    'www/lib/**/examples',
+    'www/lib/**/caching',
+    'www/lib/**/perf',
+    'www/lib/**/build',
+    'www/lib/**/doc',
+    'www/lib/**/vendor',
+    'www/lib/**/lib',
+    'www/lib/**/fp',
+    'www/lib/**/locale',
+    'www/lib/**/templates',
+
+    // . files
+    'www/lib/**/.*ignore',
+    'www/lib/**/.*rc',
+    'www/lib/**/.*.yml',
+    'www/lib/**/.bower*',
+    'www/lib/**/.bower.json',
+    'www/lib/**/.*config',
+    'www/lib/**/.*.json',
+    'www/lib/**/.*attributes',
+    'www/lib/**/.*ignore',
+    'www/lib/**/.*github',
+    'www/lib/.markdown*',
+
+    // common unnecessary files
+    'www/lib/**/package.json',
+    'www/lib/**/bower.json',
+    'www/lib/**/gruntfile.js',
+    'www/lib/**/gulp*.js',
+    'www/lib/**/CHANGELOG',
+    'www/lib/**/LICENSE',
+    'www/lib/**/LICENSE.*',
+    'www/lib/**/*.yml',
+    'www/lib/**/*.md',
+    'www/lib/**/*.map',
+
+    // specific files
+    'www/lib/moment/*',
+    'www/lib/moment/min/*',
+    '!www/lib/moment/min',
+    '!www/lib/moment/min/moment.min.js',
+
+    'www/lib/ngstorage/*',
+    '!www/lib/ngstorage/ngStorage.min.js',
+    
+    'www/lib/lodash/*',
+    'www/lib/lodash/.markdown-doctest-setup.js',
+    'www/lib/lodash/dist/*',
+    '!www/lib/lodash/dist',
+    '!www/lib/lodash/dist/lodash.min.js',
+
+    '!www/lib/ion-alpha-scroll/src',
+    '!www/lib/ion-alpha-scroll/src/ion-alpha-scroll.js',
+    '!www/lib/ion-alpha-scroll/src/ion-alpha-scroll.css',
+    
+    'www/lib/ionic/css/ionic.css',
+
+    'www/lib/ionic/js/*',
+    '!www/lib/ionic/js/ionic.bundle.min.js',
+
+    'www/lib/holderjs/*',
+    '!www/lib/holderjs/holder.min.js',
+
+    'www/lib/ionic-platform-web-client/dist/ionic.io.bundle.js',
+    'www/lib/ionic-platform-web-client/.bower.json',
+    'www/lib/ionic-platform-web-client/src',
+
+    'www/lib/ionic-ratings/**/*',
+    '!www/lib/ionic-ratings/dist',
+    '!www/lib/ionic-ratings/dist/ionic-ratings.min.js',
+    '!www/lib/ionic-ratings/src',
+    '!www/lib/ionic-ratings/src/ionic-ratings.js', 
+
+    '!www/lib/angular-holderjs/src',
+    '!www/lib/angular-holderjs/src/holder.js',
+
+    'www/lib/angular-moment/*',
+    '!www/lib/angular-moment/angular-moment.min.js',
+
+    'www/lib/ionic-datepicker/src',
+
+    'www/lib/plugin/src'
+
+  ]);
 });
